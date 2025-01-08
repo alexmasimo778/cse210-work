b3f1920207d3512c2e404eb2aa40be47d0f6f79f
alex masimo <masimoteaching@gmail.com>
2025-01-08 23:43:11 +0300

    edited week01/Exercise1/program by prompting inputs and display

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -1,9 +1,16 @@
 using System;
+using System.Threading.Tasks.Dataflow;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
-    }
+    
+        Console.Write("Write your first name " );
+        string first =Console.ReadLine();
+        Console.Write("write your last name ");
+        string last=Console.ReadLine();
+        Console.WriteLine($"I am {last} ,{first}.");
+    }    
 }
