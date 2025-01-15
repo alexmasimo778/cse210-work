c32f6d2ed74aa61013c4cfe99b0332c3c6ead901
alex masimo <masimoteaching@gmail.com>
2025-01-15 11:14:01 +0300

    wrote new line in program.cs to print/display favorite number

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -5,14 +5,14 @@ class Program
     static void Main(string[] args)
     {
 // Display Welcome Message
-        Console.WriteLine("Welcome to the Program!");
+        Console.WriteLine("You are Welcome!");
 
-// Ask for the username and return it
+// Asking for the username and return it
         Console.Write("Please enter your name: ");
         string username = Console.ReadLine();
         Console.WriteLine($"{username}");
 
-    // Asking for the user's favorite number and return it
+// Asking for the user's favorite number and return it
         Console.Write("What is your favorite number? ");
         int favoriteNumber;
         while (!int.TryParse(Console.ReadLine(), out favoriteNumber))
@@ -21,12 +21,13 @@ class Program
         }
         Console.WriteLine($"Your favorite number is {favoriteNumber}.");
 
-    // Accepting an integer as a parameter, return its square, and display it
+// Accepting an integer as a parameter, return its square, and display it
         int squaredValue = CalculateSquare(favoriteNumber);
-        Console.WriteLine($"The square of your favorite number ({favoriteNumber}) is {squaredValue}.");
+        Console.WriteLine($"Your favorite number is {favoriteNumber}");
+        Console.WriteLine($"The square of your favorite number  is {squaredValue}.");
     }
 
-    // Method to calculate the square of a number
+// Method to calculate the square of a number
     static int CalculateSquare(int number)
     {
         return number * number;
