68c8feb29df285ade83c1412976f09671b07618e
alex masimo <masimoteaching@gmail.com>
2025-02-21 16:25:24 +0300

    Fixed namespace and added missing files for exercise tracking

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,9 +1,36 @@
-using System;
+using ExerciseTracking;  
 
-class Program
+namespace ExerciseTracking
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+
+            // Create instances for each activity
+            List<Activity> activities = new List<Activity>
+            {
+                new Running("03 Nov 2022", 30, 4.8),
+                new Cycling("03 Nov 2022", 45, 22.0),
+                new Swimming("03 Nov 2022", 30, 40)
+            };
+
+            // Display summaries
+            foreach (var activity in activities)
+            {
+                Console.WriteLine(activity.GetSummary());
+            }
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
